74a29c8499b3aac5d34e72da3b4f589217be163e
thomast1906 <thomast@kainos.com>
2022-07-15 13:54:46 +0100

    test

--- pre-commit-hooks/app_gateway_backend_checks.sh
+++ pre-commit-hooks/app_gateway_backend_checks.sh
@@ -6,7 +6,7 @@ do
 
     for app_configuration_number in $(yq eval '.gateways.[].app_configuration | length' $lb_config); do
 
-        if [ "$app_configuration_number" -gt "3" ]
+        if [ "$app_configuration_number" -gt "1" ]
         then
             echo "app_configuration in $lb_config: is greater than 100, please change your product/component to use additional gateway" && exit 1
         fi
